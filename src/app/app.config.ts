3dc9a1ee0f10d5ebe3ffdb7a9caf3bb2adc9e669
H3rzius <h3rzius@gmail.com>
2023-12-13 08:48:01 +0100

    export to git

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { routes } from './app.routes';
+import {environment} from "../environments/environment";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+    ])]
+};
